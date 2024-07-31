238cea4b0ecb101a3049791b98b83ccb6576f493
Ankita Chaurasia <ankichaurasia17@gmail.com>
2024-07-31 11:47:15 +0530

    login, register, navbar

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import Navbar from '../components/Navbar'
+
+export function Home() {
+    return (
+    <>
+        <Navbar />
+        <div className='container'>
+        <h1 className='title'>Home</h1>
+        </div>
+        
+
+    </>
+    )
+}
+
+export default Home
